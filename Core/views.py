4004d34701d62731ea1ba310b1756ddd16e7dd3a
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 19:49:11 +0800

    Working with user registration with validation

--- Core/views.py
+++ Core/views.py
@@ -15,7 +15,45 @@ def Home(request):
     return render(request, 'index.html')
 
 def RegisterView(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        # getting user inputs from frontend
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_data_has_error = False
+
+        # make sure email and username are not being used
+
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, 'Username already exists')
+
+        if User.objects.filter(email=email).exists():
+            user_data_has_error = True
+            messages.error(request, 'Email already exists')
+
+        # make aure password is at least 5 characters long
+        if len(password) < 5:
+            user_data_has_error = True
+            messages.error(request, 'Password must be at least 5 characters')
+        
+        if not user_data_has_error:
+            new_user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                username = username,
+                password = password
+            )
+            messages.success(request, 'Account created. Login now')
+            return redirect('login')
+        else:
+            return redirect('register')
+
+    #return render(request, 'register.html')
 
 def LoginView(request):
     return render(request, 'login.html')
