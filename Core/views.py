3d1d429d94641e8b742f5ab23757673f19ce3427
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 16:19:04 +0800

    Initialize views.py

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.urls import reverse
+from .models import *
+
+
 
 # Create your views here.
