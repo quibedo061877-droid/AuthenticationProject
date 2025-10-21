f4008d6a22d3ba56789bb0e3367842e6f6c9a8a5
Nonicks Quibedo <nonicksquibedo@gmail.com>
2025-10-21 16:39:52 +0800

    Map views to urls

--- Core/urls.py
+++ Core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+    path('', views.Home, name='home'),
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login'),
 ]
